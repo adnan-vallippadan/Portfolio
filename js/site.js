beb677dab24c652e96f434a3aefed7e31f29fde0
Adnan Vallippadan <adnanvp9@gmail.com>
2025-01-19 09:56:40 +0530

    Added mobile nav bar

--- js/site.js
+++ js/site.js
@@ -1,5 +1,10 @@
 let site = {
-    initialize: function() {
-        
+    initialize: function() {        
+        site.setCurrentYear();
+    },
+
+    setCurrentYear: function() {
+        const year = new Date().getFullYear();
+        document.getElementById('year').textContent = year;
     }
 }
