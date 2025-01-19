a23771a7de3d584b849c94c3aec51897097ca55e
Adnan Vallippadan <adnanvp9@gmail.com>
2025-01-19 10:16:46 +0530

    Added loader

--- js/site.js
+++ js/site.js
@@ -1,6 +1,14 @@
 let site = {
     initialize: function() {        
         site.setCurrentYear();
+        site.initializeLoader();
+    },
+
+    initializeLoader: function() {
+        setTimeout(() => {
+            let element = document.getElementById("loading-animation");
+            element.style.display = 'none';
+        }, 500);
     },
 
     setCurrentYear: function() {
